b1ec7ef2243e66b5f5a747356d0503002b524d91
Samuel STEINER <samuel.steiner@nec-ingenierie.fr>
2022-01-03 10:34:08 +0000

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -11,7 +11,7 @@
         }
     }
     #LOGO_Concept {
-        filter: brightness(1);
+        filter: brightness(50%);
     }
 </style>
 <aside id="aside-newsletter" class="page-section bg-quinary text-white py-4">
