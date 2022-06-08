d0e9e3a623844e7ff4434e57c531d14324432957
Samuel STEINER <samuel.steiner@nec-ingenierie.fr>
2022-06-08 12:29:31 +0000

    Update 7 index.php

--- index.php
+++ index.php
@@ -114,6 +114,7 @@
                     <h1 class="home-header-bottom-title ttitle th4">NEW Économie de la Construction</h1>
                     <!--p class="home-header-bottom-text th5 text-uppercase">Bureau d'études techniques spécialisé dans le contrôle<br class="d-none d-md-block"> sur site des différentes réglementations thermiques</p-->
                     <p class="home-header-bottom-text th5 text-uppercase">Buerau d'économie de la construction "Tous corps d'Etats"</p>
+                    <img class="w-auto h-100" src="./images/logos/Logo_NEC_Concept.png?v=<?php echo(date("Y-m-d-H-i-s", filemtime('./images/logos/Logo_NEC_Concept.png'))) ?>" alt="" width="215" height="84">
                 </div>
             </div>
 
